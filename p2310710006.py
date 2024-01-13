2e1d432fe8d03cb0b0ea00176edb41c19526c2fb
Feichtinger Daniel <daniel.feichtinger@students.fh-wels.at>
2024-01-13 10:05:22 +0100

    Update to the code     include a code to argparser to controll if None, and fill them correctly if so

--- p2310710006.py
+++ p2310710006.py
@@ -6,7 +6,6 @@
 #import system libs
 import argparse
 import sys
-import numpy as np
 
 #import defined functions
 from plot_example import exec_plot_distance_time_ind, exec_plot_speed_time_ind
@@ -20,19 +19,37 @@ arg_parser_.add_argument("--friction", type=float, help="friction_argument")
 arg_parser_.add_argument("--incline", type=float, help="incline_argument")
 cmd_call_args_ = arg_parser_.parse_args()
 
+#Argparse controll and set if note done
+if cmd_call_args_.pdf_file_out is None:
+    cmd_call_args_.pdf_file_out = "p2310710006.pdf"
+
+if cmd_call_args_.mass is None:
+    cmd_call_args_.mass = 1000
+
+if cmd_call_args_.velocity is None:
+    cmd_call_args_.velocity = 100
+
+if cmd_call_args_.friction is None:
+    cmd_call_args_.friction = 0.85
+
+if cmd_call_args_.incline is None:
+    cmd_call_args_.incline = 0
+
+
 #===============
 # a method
 def main_method():
-  main_method.__doc__ = "sample main method"
+    main_method.__doc__ = "sample main method"
 
-  #define Variables
-  V_START = cmd_call_args_.velocity
-  Incline = cmd_call_args_.incline #Steigung in RAD
-  µ_dyn = cmd_call_args_.friction
+#define Variables
+    V_START = cmd_call_args_.velocity
+    Incline = cmd_call_args_.incline #Steigung in RAD
+    µ_dyn = cmd_call_args_.friction
+    plot_filename = cmd_call_args_.pdf_file_out
 
-  #create braking plots
-  exec_plot_speed_time_ind("Plot_speed_time_" + cmd_call_args_.pdf_file_out, V_START, µ_dyn)
-  exec_plot_distance_time_ind("Plot_distance_time_" + cmd_call_args_.pdf_file_out, V_START, µ_dyn, Incline)
+    #create braking plots
+    exec_plot_speed_time_ind("Plot_speed_time_" + plot_filename, V_START, µ_dyn )
+    exec_plot_distance_time_ind("Plot_distance_time_" + plot_filename, V_START, µ_dyn, Incline)
 
 #===============
 # do work and call a methode
--- plot_example.py
+++ plot_example.py
@@ -16,84 +16,82 @@ import numpy as np
 
 #================================================
 def exec_plot_speed_time_ind(file_name_out,v_start,µ):
-
   #define figure
-  fig = plt.figure()
+    fig = plt.figure()
   #add one plot
-  ax1 = fig.add_subplot(111)
+    ax1 = fig.add_subplot(111)
 
   #define variables
-  timestep = 0.01
-  v = [v_start] #in km/h
-  t = [0]
-  g = 9.81 #in m/s²
-
+    timestep = 0.01
+    v = [v_start] #in km/h
+    t = [0]
+    g = 9.81 #in m/s²
+  
   #calculate the data independent on timesteps
-  while v[-1] > 0:
-    t.append(t[-1] + timestep)
-    v.append(v[-1] - µ * g * timestep)  
+    while v[-1] > 0:
+      t.append(t[-1] + timestep)
+      v.append(v[-1] - µ * g * timestep)  
 
   #define plots
-  ax1.plot(t, v, color ="green", lw = 2)
+    ax1.plot(t, v, color ="green", lw = 2)
 
   #add axis label
-  ax1.set_xlabel("time [s]")
-  ax1.set_ylabel("speed [km/h]")
+    ax1.set_xlabel("time [s]")
+    ax1.set_ylabel("speed [km/h]")
 
   # Add a grid
-  ax1.grid(True)
+    ax1.grid(True)
 
   #add plot label
-  fig.suptitle("speed to time on braking \n\n", fontweight ="bold")
+    fig.suptitle("speed to time on braking \n\n", fontweight ="bold")
 
   #export as PDF
-  plt.savefig(file_name_out)
+    plt.savefig(file_name_out)
   
   #Ausgabe
-  print("Calc finished of " + file_name_out)
+    print("Calc finished of " + file_name_out)
 
 #================================================
 def exec_plot_distance_time_ind(file_name_out,v_start,µ,incline):
-
-  #define figure
-  fig = plt.figure()
+    #define figure
+    fig = plt.figure()
   #add one plot
-  ax1 = fig.add_subplot(111)
+    ax1 = fig.add_subplot(111)
 
   #define variables
-  timestep = 0.01
-  s = [0]
-  t = [0]
-  v = [v_start/3.6] #Convert km/h into m/s
-  g = 9.81
-  braking_force = µ * g * np.cos(incline)
+    timestep = 0.01
+    s = [0]
+    t = [0]
+    v = [v_start/3.6] #Convert km/h into m/s
+    g = 9.81
+    braking_force = µ * g * np.cos(incline)
 
   #calculate the data independent on timesteps
-  while v[-1] > 0:
-    t.append(t[-1] + timestep)
-    v.append(v[-1] - braking_force * timestep)
-    s.append(s[-1] + v[-1] * timestep + 0.5 * (-braking_force) * (timestep ** 2))
+    while v[-1] > 0:
+      t.append(t[-1] + timestep)
+      v.append(v[-1] - braking_force * timestep)
+      s.append(s[-1] + v[-1] * timestep + 0.5 * (-braking_force) * (timestep ** 2))
   
   #calculate distance through rule of thumb
-  s_thumb = (v_start/10)**2 /2
+    s_thumb = (v_start/10)**2 /2
 
   #define plot
-  ax1.plot(t, s, color ="green", lw = 1)
-  ax1.axhline(s_thumb, color='red', linestyle='--', label='Rule of Thumb')
+    ax1.plot(t, s, color ="green", lw = 1)
+    ax1.axhline(s_thumb, color='red', linestyle='--', label='Rule of Thumb')
 
   #add axis label
-  ax1.set_xlabel("time [s]")
-  ax1.set_ylabel("distance [m]")
+    ax1.set_xlabel("time [s]")
+    ax1.set_ylabel("distance [m]")
 
   # Add a grid
-  ax1.grid(True)
+    ax1.grid(True)
 
   #add plot label
-  fig.suptitle("braking distance \n\n", fontweight ="bold")
+    fig.suptitle("braking distance \n\n", fontweight ="bold")
 
   #export as PDF
-  plt.savefig(file_name_out)
+    plt.savefig(file_name_out)
   #Ausgabe
-  print("Calc finished of " + file_name_out)
-  print("\tcalc distance: " + str(s[-1]))
-  print("\trule of thumb: " + str(s_thumb) )
+    print("Calc finished of " + file_name_out)
+    print("\tcalc distance: " + str(s[-1]))
+    print("\trule of thumb: " + str(s_thumb) )
